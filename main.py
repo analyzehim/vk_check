50243a83b904b716bc3fbd27357b1e60b9ab64fb
Raev_e <raevegnforspam@rambler.ru>
2017-11-02 14:27:02 +0300

    Add error logging

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram_proto import Telebot
 from vk_proto import VkBot
 from sqlite_proto import Cash
-from common_proto import VkMessage, log_event
+from common_proto import VkMessage, log_event, get_exception
 import time
 
 
@@ -41,7 +41,7 @@ def exit_check(update_list): # if you wanna terminate, just text exit to bot
     return False
 
 
-def mes_check(update_list):
+def check_ans(update_list):
     if not update_list:
         return False
     for telegram_message in update_list:
@@ -57,26 +57,29 @@ if __name__ == "__main__":
     cashDB = Cash()
     telegram_bot = Telebot()
     while True:
-        update_list = []
-        while True:
-            new_update = telegram_bot.get_updates()
-            if new_update:
-                update_list += new_update
+        try:
+            update_list = []
+            while True:
+                new_update = telegram_bot.get_updates()
+                if new_update:
+                    update_list += new_update
+                else:
+                    break  # this means, what no have new updates
+
+            if exit_check(update_list):
+                    telegram_bot.send_text(telegram_bot.chat_id, "EXIT COMMAND")
+                    log_event("EXIT COMMAND")
+                    break
+
+            check_result = check_ans(update_list)
+            if check_result:
+                    recipient_id = cashDB.get_user_id(check_result['recipient_name'])
+                    text = check_result['text']
+                    vk_bot.send_text(recipient_id, text)
             else:
-                break  # this means, what no have new updates
-
-        if exit_check(update_list):
-                telegram_bot.send_text(telegram_bot.chat_id, "EXIT COMMAND")
-                log_event("EXIT COMMAND")
-                break
-
-        check_result = mes_check(update_list)
-        if check_result:
-                recipient_id = cashDB.get_user_id(check_result['recipient_name'])
-                text = check_result['text']
-                vk_bot.send_text(recipient_id, text)
-
-        for mes in get_unread_messages(vk_bot.get_mes()):
-                telegram_bot.send_text(telegram_bot.chat_id, str(mes))
-        time.sleep(vk_bot.interval)
+                for mes in get_unread_messages(vk_bot.get_mes()):
+                        telegram_bot.send_text(telegram_bot.chat_id, str(mes))
+            time.sleep(vk_bot.interval)
+        except Exception as e:
+            log_event(get_exception())
 
